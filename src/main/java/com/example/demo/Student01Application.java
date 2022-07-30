f38aa9c47c2175e0a0fca521b2edeff369647f0f
SuKirolkar <supriyakirolkar96@gmail.com>
2022-07-29 20:42:15 -0700

    changes done

--- src/main/java/com/example/demo/Student01Application.java
+++ src/main/java/com/example/demo/Student01Application.java
@@ -8,6 +8,7 @@ public class Student01Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Student01Application.class, args);
+		System.out.println("Second trail done successfully.....");
 	}
 
 }
